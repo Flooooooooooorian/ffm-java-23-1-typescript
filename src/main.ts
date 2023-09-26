15245cfda1c4975332871cff68cd7a4de5b08846
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-09-26 10:13:42 +0200

    Add arrays

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-
 console.log("Hallo")
 
 //String name = "Florian";
 //let variable-Name : Type   = Wert
-  let name          : string = "Florian"
+let name: string = "Florian"
 
 
 let money: number = 612.54
-let bool : boolean = false
+let bool: boolean = false
 
 console.log(name)
 console.log(money)
@@ -28,7 +27,7 @@ function functionName(value: string): string {
 
 console.log(functionName("test"))
 
-let mitarbeiter: {name: string, lastName: string} = {
+let mitarbeiter: { name: string, lastName: string } = {
     name: "Florian",
     lastName: "Weber"
 }
@@ -45,7 +44,58 @@ for (let i = 0; i < 10; i++) {
 
 if (3 > 5) {
     console.log("Wahr")
-}
-else {
+} else {
     console.log("Falsch")
 }
+
+
+console.log("====================================")
+
+
+function sum(value1: number, value2: number): number {
+    let result: number = value1 + value2
+
+    return result
+}
+
+const result = sum(3, 17)
+
+console.log(result)
+
+let myFunction = (value1: string): string => {
+    return value1 + " TEST"
+}
+
+console.log(myFunction("ABC"))
+
+
+let array: string[] = ["Test", "Hallo", "ABC"]
+
+console.log(array)
+
+console.log(array[0])
+
+array.push("123")
+
+console.log(array)
+
+const lastElement = array.pop()
+
+console.log(lastElement)
+console.log(array)
+
+console.log("===================")
+
+const newArray = array.map((element) => {return element.length})
+
+console.log(array)
+console.log(newArray)
+
+const filteredArray = array.filter(element => element.charAt(0) === "T")
+
+console.log(array)
+console.log(filteredArray)
+
+
+const a = null;
+const b = undefined;
